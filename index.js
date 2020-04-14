fe85e257ac015521efe5a955ea784f8dd7b13b5b
Nitipatt <8529806+Nitipatt@users.noreply.github.com>
2020-04-15 00:49:09 +0700

    Add prompt for comfortable using

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const rp = require("request-promise");
+const request = require("request");
 const $ = require("cheerio");
 const download = require("download-to-file");
+const prompt = require("prompt-sync")();
 
-function getDownloadLink(url) {
+function getDownloadLink(url, query) {
   return new Promise(async (resolve, reject) => {
     try {
       const html = await rp(url);
       resolve(
-        Object.values($(".powerpress_link_d", html))
+        Object.values($(query, html))
           .map((item) => {
             try {
               return JSON.parse(JSON.stringify(item.attribs)).href;
@@ -22,12 +24,14 @@ function getDownloadLink(url) {
     }
   });
 }
-function downloadFile(url) {
+function downloadFile(url,destination) {
   return new Promise(async (resolve, reject) => {
     try {
-      const path = "./result/"+url.split("/").slice(-1)[0]
+      const fileName = url.split("/").slice(-1)[0].split("?")[0];
+      const path = destination + fileName;
+      console.log(`Downloading ==> ${fileName}`);
       download(url, path, function (err) {
-        if (err) throw err;
+        if (err) reject(err);
         resolve(true);
       });
     } catch (e) {
@@ -35,18 +39,67 @@ function downloadFile(url) {
     }
   });
 }
-async function main() {
-  let page = 1
-  const BASE_URL = "https://www.xxx.com/page/";
-  while(true){
-    let dLink = await getDownloadLink(BASE_URL+(page++))
-    if(dLink.length == 0)break
-    dLink.forEach(element => {
-      await downloadFile(
-        element
+function getRedirectUrl(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      request(
+        {
+          url: url,
+          followRedirect: true,
+        },
+        function (error, response, body) {
+          const url =
+            response && response.request && response.request.uri
+              ? response.request.uri.href
+              : null;
+          resolve(url);
+        }
       );
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+async function execute({
+  url,
+  page = undefined,
+  query = undefined,
+  destination = undefined,
+} = {}) {
+  while (true) {
+    let dLink = await getDownloadLink(url + (page ? page++ : ""), query);
+    if (dLink.length == 0) break;
+    dLink.forEach(async (element) => {
+      try {
+        const redirect = await getRedirectUrl(element);
+        if (redirect) {
+          await downloadFile(redirect, destination);
+        }
+      } catch (e) {}
     });
   }
 }
 
-main();
+(function () {
+  let promptData = {};
+  while (!promptData["url"]) {
+    promptData["url"] = prompt(
+      "Enter URL. If there're page plese include $PAGE (eg. www.xxx.com?page=$PAGE): "
+    );
+  }
+  promptData["isPage"] = promptData["url"].includes("$PAGE");
+  promptData["url"] = promptData["url"].replace('$PAGE','')
+  while (promptData["isPage"] && !promptData["page"]) {
+    promptData["page"] = prompt("Enter starting page: ");
+  }
+  while (!promptData["query"]) {
+    promptData["query"] = prompt("Enter query selector (eg. .class > a): ");
+  }
+  promptData["destination"] = prompt(
+    "Enter result folder (default: ./result/): "
+  );
+  if(promptData["destination"])promptData["destination"] += (promptData["destination"].slice(-1)[0] != '/'?'/':'')
+  else promptData["destination"] = './result/'
+  execute(promptData);
+})();
